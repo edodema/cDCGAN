ef360c11f3ca56588a45fb301637386d884b1fe3
edodema <edodema13@gmail.com>
2022-02-11 17:49:09 +0100

    Changed channels' defualt

--- src/parser.py
+++ src/parser.py
@@ -68,7 +68,7 @@ parser.add_argument(
     "--img_size", type=int, default=64, help="Size we want to reshape our images to."
 )
 
-parser.add_argument("--channels", type=int, default=3, help="Number of channels.")
+parser.add_argument("--channels", type=int, default=1, help="Number of channels.")
 
 parser.add_argument("--num_classes", type=int, default=10, help="Number of classes.")
 
